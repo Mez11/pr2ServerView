f12bf6d2b09520b8d334ae4e4c5c77e8476d4ee0
Joscelyn Meztli <meztli.swim@gmail.com>
2016-10-24 14:55:50 -0500

    cambio de estructura

--- src/com/redes/p2/servidor/RunnerServer.java
+++ src/com/redes/p2/servidor/RunnerServer.java
@@ -11,7 +11,6 @@ public class RunnerServer {
 	public static void main( String args[] ){
 		Servidor servidor = new Servidor( );
 		ProductosDao dao;
-		int codigoOperacion;
 		/**Conexion con laBD*/
 		System.out.println( "Usando el puerto " + DEFAULT_PORT );
 		
@@ -20,35 +19,50 @@ public class RunnerServer {
 			System.out.println( "Previos errores, el envio no puede iniciar @.#.." );
 			return;
 		}
+		dao = new ProductosDao();//inicializar el objeto
+		dao.inicializarConexion( );//inicializar la conexion
+		while( true ){
+			procesarCliente( servidor, dao );
+		}//Fin ciclo escuchar clientes
+	}//fin main
+	
+	private static void procesarCliente( Servidor servidor, ProductosDao dao ){
 		try {
-			dao = new ProductosDao();//inicializar el objeto
-			dao.inicializarConexion( );//inicializar la conexion
+			//Aceptar a un cliente
+			System.out.println( "A la espera de un cliente..." );
+			servidor.bind( );
+			//Inicializar la BD
 			while( true ){
-				//Aceptar a un cliente
-				System.out.println( "A la espera de un cliente..." );
-				servidor.bind( );
-				//Inicializar la BD
-				while( true ){
-					//Obtener el codigo de operaciones
-					codigoOperacion = servidor.getCodigoOperacion( );
-					if( codigoOperacion == Operaciones.OP_CATALOGO ){
-						//Debemos enviar el catalogo del dao
-						servidor.enviar( dao.getProductos( ) );
-					}
-					else if( codigoOperacion == Operaciones.OP_COMPRA ){
-						//Implementar la logica de confirmacion compra
-					}
-					else if( codigoOperacion == Operaciones.OP_CERRAR_CONEXION ){
-						servidor.cerrar( );
-						break;
-					}
-					else{
-						System.out.println( "Codigo de operacion desconocido: " + codigoOperacion );
-					}
-				}//fin ciclo para escuchar codigos de operacion
-			}//Fin ciclo escuchar clientes
+				if( !procesarOperacion( servidor, dao ) ){
+					break;
+				}
+			}//fin ciclo para escuchar codigos de operacion
 		} catch (IOException e) {
 			e.printStackTrace();
+		}//fin try-catch
+	}
+	
+	private static boolean procesarOperacion( Servidor servidor, ProductosDao dao ) throws IOException{
+		int codigoOperacion;
+		//Obtener el codigo de operaciones
+		codigoOperacion = servidor.getCodigoOperacion( );
+		if( codigoOperacion == Operaciones.OP_CATALOGO ){
+			//Debemos enviar el catalogo del dao
+			servidor.enviar( dao.getProductos( ) );
 		}
-	}//fin main
+		else if( codigoOperacion == Operaciones.OP_COMPRA ){
+			//Implementar la logica de confirmacion compra
+		}
+		else if( codigoOperacion == Operaciones.OP_CERRAR_CONEXION ){
+			servidor.cerrar( );
+			//break;
+			return false;
+		}
+		else{
+			System.out.println( "Codigo de operacion desconocido: " + codigoOperacion );
+		}
+		//el while siga ejecutandose
+		return true;
+	}
+	
 }//fin clase
