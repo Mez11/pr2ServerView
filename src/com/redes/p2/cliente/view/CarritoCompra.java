7565877fa734088aead205d13d1222190f9d979b
Joscelyn Meztli <meztli.swim@gmail.com>
2016-10-24 15:43:26 -0500

    Carrito de compra tiene enlace con ConexionCliente

--- src/com/redes/p2/cliente/view/CarritoCompra.java
+++ src/com/redes/p2/cliente/view/CarritoCompra.java
@@ -1,7 +1,5 @@
 package com.redes.p2.cliente.view;
 
-import java.awt.EventQueue;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JButton;
@@ -12,16 +10,13 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
+import com.redes.p2.cliente.BaseDatosCarrito;
 import com.redes.p2.model.Productos;
-import com.redes.p2.servidor.dao.ProductosDao;
-
-import javax.swing.JButton;
 
 
 public class CarritoCompra {
 
 	private JFrame frmCarritoDeCompra;
-	private JTable table;
 	private JLabel lblCantidadEnEl;
 	private JTextField textField;
 	private JLabel lblIdDelProducto;
@@ -29,22 +24,10 @@ public class CarritoCompra {
 	private JButton btnModificar;
 	private JButton btnFinalizarCompra;
 	private JTable productosTable;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					CarritoCompra window = new CarritoCompra();
-					window.frmCarritoDeCompra.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	private String[] columnNames = {"Id",
+            "Nombre",
+            "Precio",
+            "Cantidad"};
 
 	/**
 	 * Create the application.
@@ -66,45 +49,6 @@ public class CarritoCompra {
 		JLabel lblProdutosEnCarrito = new JLabel("Produtos en carrito");
 		lblProdutosEnCarrito.setBounds(27, 12, 146, 15);
 		frmCarritoDeCompra.getContentPane().add(lblProdutosEnCarrito);
-		
-		/**Tabla Carrito**/
-		table = new JTable();
-		table.setBounds(33, 54, 348, 71);
-		/**JTable @.@. llenado**/
-		
-		/***/
-		
-		String[] columnNames = {"Id",
-		                        "Nombre",
-		                        "Precio",
-		                        "Cantidad"};
-		Object[][] data = {
-
-
-		     { new Integer(1),"Sombrero" ,new Double(10.0),new Integer(1),"Agregar,Modificar,Eliminar"},
-		     {new Integer(2),"Sombrero" ,new Double(10.0),new Integer(1),"Agregar,Modificar,Eliminar"},
-
-		     { new Integer(1),"Sombrero" ,new Double(10.0),new Integer(1) },
-		     {new Integer(2),"Sombrero" ,new Double(10.0),new Integer(1) }
-
-		};
-
-//Then the Table is constructed using these data and columnNames:
-	
-	JTable table = new JTable(data, columnNames);
-//	JTable(Object[][] ,data, Object[][] ,columnNames);
-
-
-	//JScrollPane scrollPane = new JScrollPane(table);
-	//table.setFillsViewportHeight(true);
-
-
-	//container.setLayout(new BorderLayout());
-	//container.add(table.getTableHeader(), BorderLayout.PAGE_START);
-	//container.add(table, BorderLayout.CENTER);
-		
-		/**Añadiendo a contentPanel*/
-		frmCarritoDeCompra.getContentPane().add(table);
 		
 		lblCantidadEnEl = new JLabel("Cantidad en el carrito");
 		lblCantidadEnEl.setBounds(27, 143, 162, 15);
@@ -133,41 +77,46 @@ public class CarritoCompra {
 		frmCarritoDeCompra.getContentPane().add(btnFinalizarCompra);
 		
 		productosTable = new JTable();
-		
-		
 		productosTable.setModel(
-				new DefaultTableModel( data, columnNames ) {
-			Class[] columnTypes = new Class[] {
-				Integer.class, String.class, Double.class, Integer.class
-			};
-			public Class getColumnClass(int columnIndex) {
-				return columnTypes[columnIndex];
-			}
+				new DefaultTableModel( null, columnNames ) {
+					/**
+					 * 
+					 */
+					private static final long serialVersionUID = 4037384032324381295L;
+					Class<?>[] columnTypes = new Class[] {
+							Integer.class, String.class, Double.class, Integer.class
+					};
+					public Class<?> getColumnClass(int columnIndex) {
+						return columnTypes[columnIndex];
+					}
 		});
 		
 		JScrollPane tableScroll = new JScrollPane( productosTable );
 		tableScroll.setBounds(37, 28, 376, 92);
 		frmCarritoDeCompra.getContentPane().add( tableScroll );
+		llenarTabla( );
+		frmCarritoDeCompra.setVisible( true );
 	}
 	
-	//Metodo para mostrar la tabla desde la BD
 	
-	public void mostrar(){
-		  
-		  DefaultTableModel modelo =(DefaultTableModel)table.getModel();
-		 ProductosDao dao =new ProductosDao();
-
-		 
-	/*	ArrayList<Productos> list = ArrayList dao.getProductos();
-		  Object[] fila = new Object [modelo.getColumnCount()];
-		  for(int i=0; i<list.size();i++){
-		    fila[0] = list.get(i).getIdProductos();
+	public void llenarTabla(){
+		Object [] fila;
+		DefaultTableModel modelo =(DefaultTableModel)productosTable.getModel( );
+		List<Productos> list = BaseDatosCarrito.getCarrito( );
+		
+		if( !BaseDatosCarrito.hayProductos( ) ){
+			return;
+		}
+		
+		fila = new Object[4];
+		for( int i = 0; i < list.size(); i++ ){
+			System.out.println( "Agregando un producto.." );
+			fila[0] = list.get( i ).getIdProductos( );
 		    fila[1] = list.get(i).getNombre();
 		    fila[2] = list.get(i).getPrecio();
-		     modelo.addRow(fila);*/
-		    
-		  }
+		    fila[3] = list.get(i).getExistencias( );
+		    modelo.addRow( fila );
 		}
 
-	
-//}
+	}//end mostrar
+}//end class
